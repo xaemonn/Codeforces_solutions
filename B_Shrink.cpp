962c44ce1466ea375457eb1e45d4adca1e5a192f
xaemonn <dishakwatra01@gmail.com>
2025-06-11 13:15:45 +0530

    icpc practice

---
+++ B_Shrink.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+void solve(){
+    ll n; cin >> n;
+    vector<ll> v,b;
+
+    int even = n / 2;
+    int odd = n / 2;
+    if(n%2){
+        odd++;
+    }
+
+    for (int i = 1; i<=n; i+=2){
+        v.push_back(i);
+    }
+
+    for (int i = 2; i <=n; i+=2){
+        b.push_back(i);
+    }
+
+    for (int i = b.size() - 1; i >= 0; i--)
+    {
+        v.push_back(b[i]);
+    }
+
+    for(auto i:v){
+        cout << i << " ";
+    }
+
+    cout << endl;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
